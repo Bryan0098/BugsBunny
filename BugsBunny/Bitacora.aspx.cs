1228340ad9170affdd5237fd0564be10b9421c74
Lios456 <luis.jerez0075@utc.edu.ec>
2024-07-17 14:37:47 -0500

    Fix: BDD

--- BugsBunny/Bitacora.aspx.cs
+++ BugsBunny/Bitacora.aspx.cs
@@ -1,57 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Services.Description;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 namespace BugsBunny
 {
     public partial class Bitacora : System.Web.UI.Page
     {
-        //Definicion de atributos
-        string cadenaConexion = "";
-        SqlConnection conexion;
+        static string cadenaConexion = ConfigurationManager.ConnectionStrings["bb_bugsbunny"].ConnectionString;
+        SqlConnection conexion = new SqlConnection(cadenaConexion);
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Abrir la conexcion a la base de datos
-            this.cadenaConexion = ConfigurationManager.ConnectionStrings["bb_bugsbunny"].ConnectionString;
-            this.conexion = new SqlConnection(cadenaConexion);
-        }
-        public bool inserta_bitacora(string nombre, DateTime nacimiento, string peso, string comportamiento, string observaciones)
-        {
-            // Validar la fecha de nacimiento
-            if (nacimiento == DateTime.MinValue || nacimiento > DateTime.Now)
-            {
-                // La fecha de nacimiento no es válida (puede ser DateTime.MinValue o una fecha futura)
-                return false;
-            }
-            // Crear un comando para ejecutar el procedimiento almacenado 
-            SqlCommand comanddoInsercion = new SqlCommand("sp_registro_bitacoras", this.conexion);
-            //Comando para ejecutar el en sqlserver
-            comanddoInsercion.CommandType = System.Data.CommandType.StoredProcedure;
-            // Pasar los parametros 
-            comanddoInsercion.Parameters.AddWithValue("@nombres", nombre);
-            comanddoInsercion.Parameters.AddWithValue("@fechas", nacimiento);
-            comanddoInsercion.Parameters.AddWithValue("@pesos", peso);
-            comanddoInsercion.Parameters.AddWithValue("@comportamientos", comportamiento);
-            comanddoInsercion.Parameters.AddWithValue("@observaciones", observaciones);
             try
             {
+                Page.Response.Write("<script>console.log('" + cadenaConexion + "');</script>");
+                consultar();
+            }
+            catch (Exception ex)
+            {
+                Page.Response.Write("<script>console.log('" + ex.Message + "');</script>");
+            }
+        }
+        public void consultar()
+        {
+            var data = new SqlDataAdapter("SELECT * FROM bitacoras", this.conexion);
+            var datos = new DataSet();
+            data.Fill(datos);
+            this.conejostb.DataSource = datos.Tables[0];
+            this.conejostb.DataBind();
+        }
+
+        public bool InsertaBitacora(string nombre, DateTime nacimiento, string peso, string comportamiento, string observaciones)
+        {
+            try
+            {
+                // Crear un comando para ejecutar el procedimiento almacenado 
+                SqlCommand comanddoInsercion = new SqlCommand("sp_registro_bitacoras", this.conexion);
+                comanddoInsercion.CommandType = System.Data.CommandType.StoredProcedure;
+                // Pasar los parámetros 
+                comanddoInsercion.Parameters.AddWithValue("@nombres", nombre);
+                comanddoInsercion.Parameters.AddWithValue("@fechas", nacimiento);
+                comanddoInsercion.Parameters.AddWithValue("@pesos", peso);
+                comanddoInsercion.Parameters.AddWithValue("@comportamientos", comportamiento);
+                comanddoInsercion.Parameters.AddWithValue("@observaciones", observaciones);
                 this.conexion.Open();
                 comanddoInsercion.ExecuteNonQuery();
+                Page.Response.Write("<script>console.log('" + "Conejo registrado" + "');</script>");
                 this.conexion.Close();
+                consultar();
                 return true;
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error: " + ex.Message);
+                Page.Response.Write("<script>console.log('" + ex.Message + "');</script>");
+                return false;
             }
-
-
-            return false;
         }
 
         protected void btnRegistrar_Click(object sender, EventArgs e)
@@ -61,22 +72,54 @@ namespace BugsBunny
             string kg = peso.Text;
             string comporta = comportamiento.Text;
             string observa = observaciones.Text;
-
             DateTime date;
 
-            // Define el formato exacto en el que esperas que la fecha sea ingresada
-            string[] formats = { "yyyy-MM-dd", "yyyy/MM/dd", "dd-MM-yyyy", "dd/MM/yyyy" };
-
-            // Intenta convertir dateString a DateTime usando los formatos definidos
-            if (DateTime.TryParseExact(dateString, formats, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out date))
+            if (DateTime.TryParse(dateString, out date))
             {
-                // La fecha se convirtió correctamente
-                this.inserta_bitacora(name, date, kg, comporta, observa);
+                Page.Response.Write("<script>console.log('" + "Cumplió" + "');</script>");
+                this.InsertaBitacora(name, date, kg, comporta, observa);
             }
             else
             {
+                Page.Response.Write("<script>console.log('" + "No cumplió" + "');</script>");
             }
         }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = nombre.Text;
+                string dateString = fecha_nacimiento.Text;
+                string kg = peso.Text;
+                string comporta = comportamiento.Text;
+                string observa = observaciones.Text;
+                DateTime date;
+
+                if (DateTime.TryParse(dateString, out date))
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('Registrando conejo:\nNombre: {name}');", true);
+                    SqlCommand comanddoInsercion = new SqlCommand("sp_registro_bitacoras", this.conexion);
+                    comanddoInsercion.CommandType = System.Data.CommandType.StoredProcedure;
+                    comanddoInsercion.Parameters.AddWithValue("@nombres", name);
+                    comanddoInsercion.Parameters.AddWithValue("@fechas", date);
+                    comanddoInsercion.Parameters.AddWithValue("@pesos", kg);
+                    comanddoInsercion.Parameters.AddWithValue("@comportamientos", comporta);
+                    comanddoInsercion.Parameters.AddWithValue("@observaciones", observa);
+                    this.conexion.Open();
+                    comanddoInsercion.ExecuteNonQuery();
+                    Page.Response.Write("<script>console.log('" + "Conejo registrado" + "');</script>");
+                    this.conexion.Close();
+                }
+                else
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", $"alert('Fecha de nacimiento inválida.');", true);
+                }
+            }
+            catch (Exception ex)
+            {
+                Page.Response.Write("<script>console.log('" + ex.Message + "');</script>");
+            }
+        }
     }
 }
